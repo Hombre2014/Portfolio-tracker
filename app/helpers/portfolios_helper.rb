5652de5ae00531c71ac7ba18fc6dbb986d3e7b48
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-07-28 20:08:27 +0200

    Moved the logic for PL from _portfolio to helper

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -9,4 +9,20 @@ module PortfoliosHelper
     @portfolio.cash == nil ? @portfolio.cash = 0 : @portfolio.cash
     @position = Position.create(portfolio_id: @portfolio.id, symbol: "Cash", quantity: @portfolio.cash, cost_per_share: 1, open_date: @portfolio.opened_date)
   end
+
+  def get_profit_loss
+    @transactions.each do |transaction|
+      if transaction.tr_type == 'Buy'
+        @tr_comm_and_fee = transaction.commission + transaction.fee
+        @buy_total += transaction.quantity * transaction.price + @tr_comm_and_fee
+        @tr_cost += @tr_comm_and_fee
+      end
+      if transaction.tr_type == 'Sell'
+        @tr_comm_and_fee = transaction.commission + transaction.fee
+        @sell_total += transaction.quantity * transaction.price - @tr_comm_and_fee
+        @tr_cost += @tr_comm_and_fee
+      end
+    end
+    @pl = @sell_total - @buy_total
+  end
 end
