8143b70c05b12f7d9745fce04e90817e34da3e6f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-08-11 11:40:35 +0200

    Minor changes in stock rendering

--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -7,22 +7,6 @@ module PositionsHelper
       stock.commission_and_fee += position.commission_and_fee
       stock.realized_profit_loss += position.realized_profit_loss
       stock.save
-      # if position.tr_type == 'Buy'
-        # @stock.shares_owned += transaction.quantity
-        # @stock.commission_and_fee += add_cost(transaction)
-        # @stock.save
-        # @portfolio.transactions_cost += add_cost(transaction)
-        # @portfolio.save
-      # elsif transaction.tr_type == 'Sell'
-        # position = @positions.where(portfolio_id: params[:portfolio_id], symbol: transaction.symbol).first
-        # @stock.realized_profit_loss += transaction.quantity * transaction.price - add_cost(transaction) - transaction.quantity * position.cost_per_share
-        # @stock.shares_owned -= transaction.quantity
-        # @stock.commission_and_fee += add_cost(transaction)
-        # @stock.save
-        # @portfolio.realized_profit_loss += transaction.quantity * transaction.price - add_cost(transaction) - transaction.quantity * position.cost_per_share
-        # @portfolio.transactions_cost += add_cost(transaction)
-        # @portfolio.save
-      # end
     else
       new_stock = Stock.create(ticker: position.symbol, transaction_id: nil, realized_profit_loss: position.realized_profit_loss, commission_and_fee: position.commission_and_fee, shares_owned: position.quantity, portfolio_id: @portfolio.id)
       new_stock.save
