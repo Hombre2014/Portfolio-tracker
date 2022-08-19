4ab8c98c1c4191cb56924b7ddf8a8d443c8f0db6
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-08-19 15:40:12 +0200

    Add a contact page and form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/about'
+  get 'pages/contact'
   resources :stocks
   devise_for :users
   resources :users, only: [:index, :show] do
