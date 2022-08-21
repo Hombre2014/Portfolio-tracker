60c8e21c4c5098586bcce6a690bd77809a46e840
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-08-21 21:34:06 +0200

    Add set_user method in User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # private
+  private
 
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-  # def user_params
-  #   params.require(:user).permit(:name)
-  # end
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
