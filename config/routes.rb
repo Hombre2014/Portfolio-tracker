d23793dc247ec4cf4031b09035734d8dea8381ce
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-11-23 21:44:39 +0100

    Allow devise_for registrations and sessions in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'pages/contact'
   resources :stocks
   devise_for :users, controllers: {
-    # registrations: 'users/registrations',
-    # sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   resources :users, only: [:index, :show] do
