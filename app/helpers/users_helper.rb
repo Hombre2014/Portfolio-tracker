8c8027830e72ed9c747c7a2f5de89041f78d8c13
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-11-24 14:41:39 +0100

    Fix an error with Gmail avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,6 +2,6 @@ module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar", style: "border-radius: 50%;")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", style: "max-width: 32px; hight: auto; margin: auto; margin-right: 1rem; border-radius: 50%;")
   end
 end
