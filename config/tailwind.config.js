c4e9d185747e5fa32e422b46ad4b10518e054b8a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2022-12-07 16:03:42 +0100

    Change the colors for buttons hover and remove Delete and edit transactions buttons.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,10 +20,10 @@ module.exports = {
           saturated: '#82c65a',
           inverted: '#7a449a',
           grayscale: '#909090',
-          complement: '#2D0057', // used to be #923ae6
+          complement: '#6847A0', // used to be #923ae6
           split_blue: '#001E57', // used to be #216dff
           triad_red: '#ff214b',
-          triad_blue: '#320061', // used to be #21c2ff, then #0F1D38, #1D3159, #4C0094
+          triad_blue: '#00571E', // used to be #21c2ff, then #0F1D38, #1D3159, #4C0094
           square_red: '#f95427',
           darker: '#68a047',
           square_blue: '#21daff',
