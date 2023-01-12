914a1f3db0ad904e07700ed286ac3cecb7196e01
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-01-12 19:27:05 +0100

    Fix an error iwth transaction_type not defined

--- app/javascript/controllers/forms_controller.js
+++ app/javascript/controllers/forms_controller.js
@@ -17,24 +17,29 @@ export default class extends Controller {
   }
 
   transactionTypeCondition(event) {
-    const transaction_type = document.getElementById('transaction_product_type');
-    transaction_type.addEventListener('change', this.handleChange);
+    // const transaction_type = document.getElementById('transaction_product_type');
+    // transaction_type.addEventListener('change', this.handleChange);
 
-
-    console.log('Transaction type: ', transaction_type.event.target.value);
-    transaction_type.event.target.value === 'Cash in' || transaction_type.event.target.value === 'Cash out'
+    // console.log('Transaction type: ', transaction_type);
+    // console.log('Transaction type: ', transaction_type.event.target.value);
+    event.target.value === 'Cash in' || event.target.value === 'Cash out'
   }
 
-  handleChange() {
-    if (this.toggleableSymbolTarget.classList.contains('hidden') || transactionTypeCondition(event)) {
-      hideOrShowFields;
-    }
-  }
-
-  hideOrShowFields() {
+  handleChange(event) {
+    console.log('Event target value: ', event.target.value);
+    if (this.toggleableSymbolTarget.classList.contains('hidden') || this.transactionTypeCondition(event)) {
+    console.log('We are here');
     this.toggleableSymbolTarget.classList.toggle('hidden')
     this.toggleableQuantityTarget.classList.toggle('hidden')
     this.toggleableCommissionTarget.classList.toggle('hidden')
     this.toggleableFeeTarget.classList.toggle('hidden')
+    }
   }
+
+  // hideOrShowFields() {
+  //   this.toggleableSymbolTarget.classList.toggle('hidden')
+  //   this.toggleableQuantityTarget.classList.toggle('hidden')
+  //   this.toggleableCommissionTarget.classList.toggle('hidden')
+  //   this.toggleableFeeTarget.classList.toggle('hidden')
+  // }
 }
