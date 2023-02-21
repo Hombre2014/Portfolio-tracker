b1983483f4d6fcf2ea0ea782d861ddfb3535afd8
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-21 20:49:33 +0100

    Changing the host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'https://yuriy-portfolio-tracker.herokuapp.com/', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'yuriy-portfolio-tracker.herokuapp.com', protocol: 'https' }
   config.action.mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
