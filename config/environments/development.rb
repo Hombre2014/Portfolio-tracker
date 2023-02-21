c76aa58eb57dc2cc8f739f72e9dc156a237a88e9
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-21 14:40:37 +0100

    Add SMTP for sending emals and edit the reset password page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -70,6 +70,19 @@ Rails.application.configure do
 
   # Add this after installing devise gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            'chamkoriyski@gmail.com',
+    password:             'hucjrailayjtytqt',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
 
   # # Adding config for bullet gem
   # config.after_initialize do
