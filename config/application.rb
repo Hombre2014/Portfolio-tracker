11a11e72ed0d3e93a459101e16c4c8c05d96b67f
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-21 23:26:43 +0100

    Change assets.compile to true in production.rb

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,6 @@ module PortfolioTracker
     config.generators.system_tests = nil
 
     # Add this, because I had a precompiled assets error trying to deploy on Heroku:
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
