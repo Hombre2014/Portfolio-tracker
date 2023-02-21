bb608f16baec2a072e9772cbda71945e557b2cce
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-22 00:38:57 +0100

    Add config.hosts to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts << "yuriy-portfolio-tracker.herokuapp.com"
   config.action_mailer.default_url_options = { host: 'yuriy-portfolio-tracker.herokuapp.com', protocol: 'https' }
   config.action.mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
