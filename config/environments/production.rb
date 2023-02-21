ed5b14f89857ce465d0a663ede7f2e6c20759a1a
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-21 20:19:43 +0100

    Made confirmable email in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,20 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { host: 'https://yuriy-portfolio-tracker.herokuapp.com/', protocol: 'https' }
+  config.action.mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'localhost:3000',
+    user_name:            Rails.application.credentials.dig(:gmail, :username),
+    password:             Rails.application.credentials.dig(:gmail, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -66,7 +80,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
