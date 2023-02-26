7627ef018211f89190f3f1f0f0c1c5cd6841b28d
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-02-26 19:29:50 +0100

    Edit profile implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @portfolios = @user.portfolios
   end
 
   private
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar", style: "margin: auto; margin-right: 1rem; border-radius: 50%;")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id} || https://www.gravatar.com/avatar/00000000000000000000000000000000?d=https://img.icons8.com/external-flaticons-flat-flat-icons/256/external-investor-home-based-business-flaticons-flat-flat-icons.png"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar avatar")
   end
 end
